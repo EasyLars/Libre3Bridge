f11b0ae07ce24854688548bd8d8147b51db7b0c2
Easy Famliy <lofrola@outlook.com>
2026-06-12 10:25:37 +0200

    Initial Commit

---
+++ Libre3Test/Libre3TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Libre3TestApp.swift
+//  Libre3Test
+//
+//  Created by Lars Oeljeschläger on 12.06.26.
+//
+
+import SwiftUI
+
+@main
+struct Libre3TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
